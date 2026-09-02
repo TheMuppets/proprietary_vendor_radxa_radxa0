e390dc0d9ad8fe821f7a4c4eb6f58d08b621cea1
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 11:52:17 -0400

    radxa0: Update bootloader image prebuilts
    * Old toolchains are no longer available without sign-in,
      update to use public ones.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter radxa0 radxa0_car radxa0_tab,$(TARGET_DEVICE)),)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,1a809438fdb44ef5547f1efb13fbd2ffc032cf14)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,0b84038d998ba1308333eac44f24a6279ca2a34b)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,cf4485ed64d6785e24f85fbd87ed9a5390d72970)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,2b0c4cba05fec7a33254a33773ff31f9f554ea0e)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,a18966f288dd1c407e511fabb444c4ef13283670)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,773b2aff475207b291e6413e660de89a50077c04)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
