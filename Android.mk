81079bf72281da96250ef4c7755ba2587349df95
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-12 20:34:38 -0400

    radxa0: Update bootloader image prebuilt
    Generated as of fd4a7d45096f5f66f9afba13dacfa29c271aaf47
    
    Change-Id: Ia8c64406cd183944681f5a2be0f0b83ef81fa7f2

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter radxa0 radxa0_car radxa0_tab,$(TARGET_DEVICE)),)
 
-$(call add-radio-file-sha1-checked,radio/bootloader.img,c0752b066bb62c3e822ba3d79e68aec73b1107dc)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,63bd4e92141686588132912a85e8f9a9844774e5)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
